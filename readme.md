7e63aa4df5312e10755852045def965d4ebd799b
quist <joakiml@gmail.com>
2016-01-22 00:41:25 +0100

    Initial commit with a client accesing the provided url

---
+++ readme.md
@@ -0,0 +1,2 @@
+java -Dhttp.proxyHost=localhost -Dhttp.proxyPort=3001 -Dhttp.nonProxyHosts= -jar target/client-1.0-SNAPSHOT.jar url`
+
---
+++ src/main/java/Client.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Client {
+
+    public static void main(String args[]) throws IOException {
+        if (args.length < 1) {
+            System.out.println("Usage: client serverUrl");
+            System.exit(1);
+        }
+
+        String url = args[0];
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+    }
+}
